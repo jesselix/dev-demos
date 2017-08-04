49932753434ea16f6234d82ca3fda49f2326c2ca
Junying Li <jesselix@live.com>
2017-08-04 16:02:26 +0800

    =

---
+++ ZzJavaDev/src/main/java/li/jesse/thread/MyThread.java
@@ -0,0 +1,15 @@
+package li.jesse.thread;
+
+public class MyThread extends Thread
+{
+    private int i = 0;
+
+    @Override
+    public void run()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
---
+++ ZzJavaDev/src/test/java/li/jesse/ThreadTest.java
@@ -0,0 +1,7 @@
+package li.jesse;
+
+// http://www.cnblogs.com/lwbqqyumidi/p/3804883.html
+
+public class ThreadTest
+{
+}
