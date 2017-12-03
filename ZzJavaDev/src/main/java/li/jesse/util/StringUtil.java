fecd4355f5befa7f5d234a42cad3c8158c3c7f30
Junying Li <jesselix@live.com>
2017-12-03 18:13:11 +0800

    =

---
+++ ZzJavaDev/src/main/java/li/jesse/util/StringUtil.java
@@ -0,0 +1,9 @@
+package li.jesse.util;
+
+public class StringUtil {
+
+
+    public static String commentSeparator(String title, String separatorElement, int maxSeparatorRadius) {
+        return null;
+    }
+}
