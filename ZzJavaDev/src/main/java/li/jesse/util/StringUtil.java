78a7f411076f756aa88824c1386d09434fa30d0e
Junying Li <jesselix@live.com>
2017-12-12 17:23:53 +0800

    =

--- ZzJavaDev/src/main/java/li/jesse/util/StringUtil.java
+++ ZzJavaDev/src/main/java/li/jesse/util/StringUtil.java
@@ -3,7 +3,15 @@ package li.jesse.util;
 public class StringUtil {
 
 
-    public static String commentSeparator(String title, String separatorElement, int maxSeparatorRadius) {
+    public static String commentSeparator(String title, String separatorElement, int maxOneSideCount) {
+        int titleLenght = title.length();
+        if (titleLenght == 0 || title == null || separatorElement.length() == 0 || separatorElement == null) {
+            return "invalid string";
+        }
+        if (maxOneSideCount < 1) {
+            return "invalid maxOneSideCount";
+        }
+
         return null;
     }
 }
