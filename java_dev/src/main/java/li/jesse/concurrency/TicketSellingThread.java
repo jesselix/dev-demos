b067d5a6d00138a353e399a7ab0da12d80a48c0d
Junying Li <jesselix@live.com>
2018-02-20 10:15:38 +0800

    =

---
+++ java_dev/src/main/java/li/jesse/concurrency/TicketSellingThread.java
@@ -0,0 +1,44 @@
+package li.jesse.concurrency;
+
+public class TicketSellingThread extends Thread {
+
+    private int ticketCount = 100;
+    private Object mutex = new Object();
+
+    public void sellTicket() {
+        synchronized (mutex) {
+            if (ticketCount > 0) {
+                ticketCount--;
+                System.out.println(Thread.currentThread().getName()
+                        + "正在卖票,还剩" + ticketCount + "张票");
+            }
+            else {
+                System.out.println("票已经卖完！");
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        while (ticketCount > 0) {
+            sellTicket();
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] arg) {
+        TicketSellingRunnable myRunnable = new TicketSellingRunnable();
+        Thread t1 = new Thread(myRunnable, "窗口1");
+        Thread t2 = new Thread(myRunnable, "窗口2");
+        Thread t3 = new Thread(myRunnable, "窗口3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
