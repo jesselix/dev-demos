925b17ff7cfbf5c4735c237218aaad92848d0c89
Junying Li <jesselix@live.com>
2018-04-13 10:14:16 +0800

    =

--- java-dev-spring/src/main/java/li/jesse/controller/HelloController.java
+++ java-dev-spring/src/main/java/li/jesse/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
