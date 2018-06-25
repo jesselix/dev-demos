2fcd11c099bea954945dd8e08db7dde80e933e17
Junying Li <jesselix@live.com>
2018-06-25 11:18:49 +0800

    =

--- java-dev-core/src/test/java/li/jesse/data/collection/set/HashSetTest.java
+++ java-dev-core/src/test/java/li/jesse/data/collection/set/HashSetTest.java
@@ -1,5 +1,8 @@
 package li.jesse.data.collection.set;
 
+import li.jesse.entity.Person;
+import org.junit.Test;
+
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -20,5 +23,21 @@ public class HashSetTest {
             System.out.printf("iterator : %s\n", iterator.next());
         }
     }
+
+    @Test
+    public void testCheckHashCode() {
+        Set<Person> set = new HashSet<>();
+
+        Person p1 = new Person("Andy", 1983);
+        Person p2 = new Person("Andy", 1983);
+
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+
+        set.add(p1);
+        set.add(p2);
+        System.out.println("size: " + set.size());
+    }
+
 
 }
