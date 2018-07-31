b88df38ffadde7dfd4b3eb94258feb63d10469cc
Junying Li <jesselix@live.com>
2018-07-31 10:38:37 +0800

    =

--- java-dev-core/src/test/java/li/jesse/basic/TimeTest.java
+++ java-dev-core/src/test/java/li/jesse/basic/TimeTest.java
@@ -2,6 +2,7 @@ package li.jesse.basic;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,8 +10,15 @@ public class TimeTest {
 
     @Test
     public void testTime() {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // 设置日期格式
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String string = df.format(new Date()).toString();
-        System.out.println(string); // new Date()为获取当前系统时间
+        System.out.println(string);
+    }
+
+    @Test
+    public void testTime2() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date date = sdf.parse("20111212133712");
+        System.out.println(date.getHours());
     }
 }
