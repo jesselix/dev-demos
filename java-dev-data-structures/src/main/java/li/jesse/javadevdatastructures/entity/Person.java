9bfd75817da4ac663637d0cd559e32d3a741df93
Junying Li <jesselix@live.com>
2018-12-23 16:53:59 +0800

    =

---
+++ java-dev-data-structures/src/main/java/li/jesse/javadevdatastructures/entity/Person.java
@@ -0,0 +1,14 @@
+package li.jesse.javadevdatastructures.entity;
+
+public class Person {
+
+    private String name;
+    private int yearOfBirth;
+
+    public Person(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+
+}
---
+++ java-dev-data-structures/src/test/java/li/jesse/javadevdatastructures/collection/set/HashSetTest.java
@@ -0,0 +1,27 @@
+package li.jesse.javadevdatastructures.collection.set;
+
+import li.jesse.javadevdatastructures.entity.Person;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetTest {
+
+    Set hashSet = new HashSet();
+
+    @Test
+    public void testCheckInsert() {
+        Set<Person> set = new HashSet<>();
+
+        Person p1 = new Person("Andy", 1983);
+        Person p2 = new Person("Andy", 1983);
+
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+
+        set.add(p1);
+        set.add(p2);
+        System.out.println("size: " + set.size());
+    }
+}
