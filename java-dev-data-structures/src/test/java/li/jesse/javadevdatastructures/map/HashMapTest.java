24f89ea3f9cf0b10074a5fdeb8708bbf6cceaf6d
Junying Li <jesselix@live.com>
2019-01-01 13:32:11 +0800

    =

---
+++ java-dev-data-structures/src/test/java/li/jesse/javadevdatastructures/map/HashMapTest.java
@@ -0,0 +1,35 @@
+package li.jesse.javadevdatastructures.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HashMapTest {
+
+    HashMap hashMap = new HashMap();
+
+    @Test
+    public void testHashMap() {
+        hashMap.put("one", 10);
+        hashMap.put("two", 20);
+        hashMap.put("three", 30);
+
+        System.out.println(hashMap);
+
+        Iterator iter = hashMap.entrySet().iterator();
+        while(iter.hasNext()) {
+            Map.Entry entry = (Map.Entry)iter.next();
+            System.out.println("next : "+ entry.getKey() +" - "+entry.getValue());
+        }
+
+        System.out.println(hashMap.size());
+
+        System.out.println("contains key two : " + hashMap.containsKey("two"));
+        System.out.println("contains key two : " + hashMap.containsKey("five"));
+
+        hashMap.clear();
+        System.out.println("is empty: " + hashMap.isEmpty());
+    }
+}
