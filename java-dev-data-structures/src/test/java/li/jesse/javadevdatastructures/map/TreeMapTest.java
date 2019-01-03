38ce3cbb7271255c4eefe56b50fb6c5417c7e8f5
Junying Li <jesselix@live.com>
2019-01-03 10:50:20 +0800

    =

--- java-dev-data-structures/src/test/java/li/jesse/javadevdatastructures/map/TreeMapTest.java
+++ java-dev-data-structures/src/test/java/li/jesse/javadevdatastructures/map/TreeMapTest.java
@@ -7,6 +7,7 @@ import java.util.TreeMap;
 public class TreeMapTest {
 
     private TreeMap treeMap1 = new TreeMap();
+    private TreeMap treeMap2 = new TreeMap();
 
     @Test
     public void testTreeMap() {
@@ -16,5 +17,17 @@ public class TreeMapTest {
 
         System.out.println(treeMap1);
         System.out.println(treeMap1.size());
+    }
+
+    @Test
+    public void testNav() {
+        treeMap2.put("aaa", 111);
+        treeMap2.put("bbb", 222);
+        treeMap2.put("ccc", 333);
+        treeMap2.put("ddd", 444);
+        treeMap2.put("eee", 555);
+
+        System.out.println(treeMap2.firstEntry());
+        System.out.println(treeMap2.lowerKey("ccc"));
     }
 }
