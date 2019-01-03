d0644985e13044277dd27bc9d3b2a4b2de5bcd01
Junying Li <jesselix@live.com>
2019-01-03 09:50:42 +0800

    =

---
+++ java-dev-spring/src/main/java/li/jesse/javadevspring/JavaDevSpringApplication.java
@@ -0,0 +1,14 @@
+package li.jesse.javadevspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevSpringApplication.class, args);
+    }
+
+}
+
