ddfd2d42f88ae30b3ec804c5ebd5b736e29049cf
Junying Li <jesselix@live.com>
2019-04-13 20:10:40 +0800

    +

--- java-dev-message-queue/src/main/java/li/jesse/javadevmessagequeue/rabbitmq/MqReceiver.java
+++ java-dev-message-queue/src/main/java/li/jesse/javadevmessagequeue/rabbitmq/MqReceiver.java
@@ -1,6 +1,7 @@
 package li.jesse.javadevmessagequeue.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class MqReceiver {
 
-    @RabbitListener(queues = "myQueue")
+    @RabbitListener(queuesToDeclare = @Queue("myQueue"))
     public void process(String message) {
 //        log.info("hello {}", message);
-        System.out.println("hello " + message);
+        System.out.println("hellooooooo " + message);
     }
 }
--- java-dev-message-queue/src/test/java/li/jesse/javadevmessagequeue/rabbitmq/simple/SimpleSenderTest.java
+++ java-dev-message-queue/src/test/java/li/jesse/javadevmessagequeue/rabbitmq/simple/SimpleSenderTest.java
@@ -1,33 +1,27 @@
-package li.jesse.javadevmessagequeue.rabbitmq;
+package li.jesse.javadevmessagequeue.rabbitmq.simple;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Date;
-
 @Component
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MqSenderTest {
-
-    // http://127.0.0.1:15672
+public class SimpleSenderTest {
 
     @Autowired
-    private AmqpTemplate amqpTemplate;
+    private RabbitTemplate template;
 
     @Test
     public void send() {
-        amqpTemplate.convertAndSend("myQueue", "now " + new Date());
-    }
-
-
-    public void send1() {
-
+        String message = "hello world";
+        template.convertAndSend("ww", message);
+        System.out.println("sent...");
     }
 
 }
